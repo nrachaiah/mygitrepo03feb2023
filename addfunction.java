6877f773cd083e788839e13e154177dc181eeaf1
rajnethi <nrachaiah@gmail.com>
2023-02-06 20:19:54 +0530

    my tast is completed

--- addfunction.java
+++ addfunction.java
@@ -1,2 +1,10 @@
 this is my addfumction file
+<<<<<<< Updated upstream
 this is my manager work is completed
+=======
+1st day of java code is completed
+2nd day of java code is completed
+3rd day of java code is completed
+4th day of java code is completed
+5th day of java code is completed
+>>>>>>> Stashed changes
